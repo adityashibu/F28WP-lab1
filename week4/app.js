0dcb0c33cc4afa6b35303ec8e543f1209928b75b
Aditya Shibu <as2397@hw.ac.uk>
2023-11-04 21:50:29 +0400

    Updated app.js

--- week4/app.js
+++ week4/app.js
@@ -1,7 +1,7 @@
 let button = document.getElementById("btn");
-const APIKEY = "bee0068e828c2ebe12b0b8eba9a6348d";
+const APIKEY = "7e3814742feaa8db91bc56c1e6b14425&units";
 
-button.addEventListener("click", function(e) {
+button.addEventListener("click", function (e) {
     e.preventDefault();
     const inputcity = document.getElementById("cityInput").value;
     getweather(inputcity);
@@ -9,13 +9,27 @@ button.addEventListener("click", function(e) {
 
 function getweather(inputcity) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputcity}&APPID=7e3814742feaa8db91bc56c1e6b14425&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-        const weatherinfo = document.getElementById("weather-info");
+        .then((response) => response.json())
+        .then((data) => {
+            const weatherinfo = document.getElementById("weather-info");
 
-        weatherinfo.textContent = `Country: ${inputcity}, Temperature: ${data.main.temp}°C, Weather: ${data.weather[0].description}, Wind: ${data.wind.speed}m/s`;
-    })
-    .catch(error => {
-        console.log(error);
-    });
+            // Create a new div element for each weather display
+            const weatherDiv = document.createElement("div");
+
+            // Set the content for the weather display
+            weatherDiv.innerHTML = `Country: ${inputcity}, Temperature: ${data.main.temp}°C, Weather: ${data.weather[0].description}, Wind: ${data.wind.speed}m/s`;
+
+            // Add an <hr> element after each weather display
+            weatherDiv.innerHTML += "<hr>";
+
+            // Append the weather display to the weatherinfo container
+            weatherinfo.insertBefore(weatherDiv, weatherinfo.firstChild);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    if (inputcity === "") {
+        alert("Please enter a city");
+    }
 }
