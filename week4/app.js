537fdb41b3e099b9ee268a60d64ac956f7fd33d5
Aditya Shibu <as2397@hw.ac.uk>
2023-11-04 22:05:48 +0400

    Updated app.js

--- week4/app.js
+++ week4/app.js
@@ -1,5 +1,4 @@
 let button = document.getElementById("btn");
-const APIKEY = "7e3814742feaa8db91bc56c1e6b14425&units";
 
 button.addEventListener("click", function (e) {
     e.preventDefault();
@@ -12,17 +11,9 @@ function getweather(inputcity) {
         .then((response) => response.json())
         .then((data) => {
             const weatherinfo = document.getElementById("weather-info");
-
-            // Create a new div element for each weather display
             const weatherDiv = document.createElement("div");
-
-            // Set the content for the weather display
             weatherDiv.innerHTML = `Country: ${inputcity}, Temperature: ${data.main.temp}°C, Weather: ${data.weather[0].description}, Wind: ${data.wind.speed}m/s`;
-
-            // Add an <hr> element after each weather display
             weatherDiv.innerHTML += "<hr>";
-
-            // Append the weather display to the weatherinfo container
             weatherinfo.insertBefore(weatherDiv, weatherinfo.firstChild);
         })
         .catch((error) => {
